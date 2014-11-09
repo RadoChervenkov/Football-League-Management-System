228a3881e8ba82cbe3e8e59e8a4c1ab30781053f
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-11-10 00:53:31 +0200

    Created empty data and web projects

---
+++ Source/FootballLeagueManagementSystem/Web/FLMS.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FLMS.Web.MvcApplication" Language="C#" %>
---
+++ Source/FootballLeagueManagementSystem/Web/FLMS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FLMS.Web.Startup))]
+namespace FLMS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
