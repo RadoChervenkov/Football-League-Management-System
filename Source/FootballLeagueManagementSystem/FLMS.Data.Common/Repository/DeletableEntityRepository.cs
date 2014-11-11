fea7eb2e435699a1d8424c7860317082eb8b4447
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-11-11 12:29:04 +0200

    Added GenericRepository and DeletableEntityRepository

---
+++ Source/FootballLeagueManagementSystem/FLMS.Data.Common/Repository/DeletableEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace FLMS.Data.Common.Repository
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using FLMS.Data.Common.Models;
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext context) : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
